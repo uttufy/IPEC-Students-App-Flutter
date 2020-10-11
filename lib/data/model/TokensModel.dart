594fddfe5ec63c9d66b1328b0bdd418b3da547b2
Utkarsh Sharma <utkarshstudent1@gmail.com>
2020-10-11 10:05:51 +0530

    [Added]  Token Parsing added

--- lib/data/model/TokensModel.dart
+++ lib/data/model/TokensModel.dart
@@ -5,9 +5,28 @@ class Tokens {
   String _viewState;
   String _viewStateGenerator;
   String _eventValidation;
+
   set cookies(String c) {
     _cookies = c;
   }
 
+  set viewState(String c) {
+    _viewState = c;
+  }
+
+  set viewStateGenerator(String c) {
+    _viewStateGenerator = c;
+  }
+
+  set eventValidation(String c) {
+    _eventValidation = c;
+  }
+
   get cookies => _cookies;
+
+  get viewState => _viewState;
+
+  get viewStateGenerator => _viewStateGenerator;
+
+  get eventValidation => _eventValidation;
 }
--- lib/data/repo/auth.dart
+++ lib/data/repo/auth.dart
@@ -1,3 +1,4 @@
+import 'package:html/parser.dart';
 import 'package:ipecstudents/data/model/GeneralResponse.dart';
 import 'package:ipecstudents/data/model/TokensModel.dart';
 import 'package:ipecstudents/data/service/webClientService.dart';
@@ -7,12 +8,24 @@ class Auth {
 
   Tokens _tokens = Tokens();
 
-  //    Handle Login
+  //  Handle Login
+
+  //  Step 1: Get Cookies
   Future<GeneralResponse> login(String username, String password) async {
     GeneralResponse response = await _webClient.getLoginToken();
 
     if (response.status) {
       _tokens.cookies = response.data.headers.map['set-cookie'].elementAt(0);
+
+      //  Step 2: Prepare formData for Login
+      var document = parse(response.data.data);
+
+      _tokens.viewState =
+          document.querySelector('#__VIEWSTATE').attributes['value'];
+      _tokens.viewStateGenerator =
+          document.querySelector('#__VIEWSTATEGENERATOR').attributes['value'];
+      _tokens.eventValidation =
+          document.querySelector('#__EVENTVALIDATION').attributes['value'];
     } else
       return response;
   }
