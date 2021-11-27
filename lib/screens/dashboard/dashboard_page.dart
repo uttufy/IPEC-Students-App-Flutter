8cae58bf7d8a932dc602287148f1d577f562ccf7
Utkarsh Sharma <utkarshstudent1@gmail.com>
2021-11-27 14:13:48 +0530

    add upgrade check

--- lib/screens/dashboard/dashboard_page.dart
+++ lib/screens/dashboard/dashboard_page.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_share/flutter_share.dart';
 import 'package:provider/provider.dart';
+import 'package:upgrader/upgrader.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../data/repo/auth.dart';
@@ -55,132 +56,139 @@ class _DashboardScreenState extends State<DashboardScreen> {
         return ScaffoldMessenger(
           key: _scaffoldKey,
           child: Scaffold(
-            body: Background(
-              child: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          CircleAvatar(
-                            radius: SizeConfig.widthMultiplier * 7,
-                            backgroundImage: MemoryImage(
-                              base64Decode(
-                                  auth.user.img.toString().split(',')[1]),
+            body: UpgradeAlert(
+              dialogStyle: UpgradeDialogStyle.cupertino,
+              // debugLogging: true,
+              // debugAlwaysUpgrade: true,
+              shouldPopScope: () => true,
+              child: Background(
+                child: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CircleAvatar(
+                              radius: SizeConfig.widthMultiplier * 7,
+                              backgroundImage: MemoryImage(
+                                base64Decode(
+                                    auth.user.img.toString().split(',')[1]),
+                              ),
+                            ),
+                            PopupMenuButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.all(
+                                        Radius.circular(20.0))),
+                                icon: ImageIcon(AssetImage(
+                                  'assets/icons/menu.png',
+                                )),
+                                onSelected: (value) {
+                                  switch (value) {
+                                    case 0:
+                                      setState(() {
+                                        if (isDark) {
+                                          AdaptiveTheme.of(context).setLight();
+                                        } else {
+                                          AdaptiveTheme.of(context).setDark();
+                                        }
+                                      });
+                                      break;
+                                    case 1:
+                                      _launchURL(appLink);
+
+                                      break;
+                                    case 2:
+                                      FlutterShare.share(
+                                          title: 'IPEC Student\'s App',
+                                          text:
+                                              'Best app for checking attendance and notices.',
+                                          linkUrl: appLink,
+                                          chooserTitle: 'IPEC');
+                                      break;
+                                    case 3:
+                                      auth.logout().then((value) =>
+                                          Navigator.pushReplacementNamed(
+                                            context,
+                                            SplashScreen.ROUTE,
+                                          ));
+
+                                      break;
+                                  }
+                                },
+                                itemBuilder: _popOptions),
+                          ],
+                        ),
+                        kHighPadding,
+                        Text(
+                          'Hello,',
+                          style: Theme.of(context).textTheme.headline5.copyWith(
+                              color: isDark ? Colors.white : Colors.black),
+                        ),
+                        Text(
+                          auth.user.name,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.headline5.copyWith(
+                              color: isDark ? Colors.white : Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Expanded(
+                          child: SingleChildScrollView(
+                            physics: BouncingScrollPhysics(),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                kHighPadding,
+                                kHighPadding,
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    optionIcon(
+                                        'assets/icons/Bag.png',
+                                        'Attendance',
+                                        () => Navigator.pushNamed(
+                                            context, AttendancePage.ROUTE)),
+                                    optionIcon(
+                                      'assets/icons/Atom.png',
+                                      'Notices',
+                                      () => Navigator.pushNamed(
+                                          context, NoticesScreen.ROUTE),
+                                    ),
+                                  ],
+                                ),
+                                kMedPadding,
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    optionIcon('assets/icons/conversation.png',
+                                        'Cafeteria\nTalks', () {
+                                      if (auth.user.isFirstYear)
+                                        _showSnackBar(
+                                            "Sorry, First year students not allowed!");
+                                      else {
+                                        // auth.reloadHuser();
+                                        Navigator.pushNamed(
+                                            context, HangoutScreen.ROUTE);
+                                      }
+                                    }),
+                                    optionIcon(
+                                        'assets/icons/Compass.png', 'About',
+                                        () {
+                                      Navigator.pushNamed(
+                                          context, AboutScreen.ROUTE);
+                                    }),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
-                          PopupMenuButton(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(20.0))),
-                              icon: ImageIcon(AssetImage(
-                                'assets/icons/menu.png',
-                              )),
-                              onSelected: (value) {
-                                switch (value) {
-                                  case 0:
-                                    setState(() {
-                                      if (isDark) {
-                                        AdaptiveTheme.of(context).setLight();
-                                      } else {
-                                        AdaptiveTheme.of(context).setDark();
-                                      }
-                                    });
-                                    break;
-                                  case 1:
-                                    _launchURL(appLink);
-
-                                    break;
-                                  case 2:
-                                    FlutterShare.share(
-                                        title: 'IPEC Student\'s App',
-                                        text:
-                                            'Best app for checking attendance and notices.',
-                                        linkUrl: appLink,
-                                        chooserTitle: 'IPEC');
-                                    break;
-                                  case 3:
-                                    auth.logout().then((value) =>
-                                        Navigator.pushReplacementNamed(
-                                          context,
-                                          SplashScreen.ROUTE,
-                                        ));
-
-                                    break;
-                                }
-                              },
-                              itemBuilder: _popOptions),
-                        ],
-                      ),
-                      kHighPadding,
-                      Text(
-                        'Hello,',
-                        style: Theme.of(context).textTheme.headline5.copyWith(
-                            color: isDark ? Colors.white : Colors.black),
-                      ),
-                      Text(
-                        auth.user.name,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.headline5.copyWith(
-                            color: isDark ? Colors.white : Colors.black,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Expanded(
-                        child: SingleChildScrollView(
-                          physics: BouncingScrollPhysics(),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              kHighPadding,
-                              kHighPadding,
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  optionIcon(
-                                      'assets/icons/Bag.png',
-                                      'Attendance',
-                                      () => Navigator.pushNamed(
-                                          context, AttendancePage.ROUTE)),
-                                  optionIcon(
-                                    'assets/icons/Atom.png',
-                                    'Notices',
-                                    () => Navigator.pushNamed(
-                                        context, NoticesScreen.ROUTE),
-                                  ),
-                                ],
-                              ),
-                              kMedPadding,
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  optionIcon('assets/icons/conversation.png',
-                                      'Cafeteria\nTalks', () {
-                                    if (auth.user.isFirstYear)
-                                      _showSnackBar(
-                                          "Sorry, First year students not allowed!");
-                                    else {
-                                      // auth.reloadHuser();
-                                      Navigator.pushNamed(
-                                          context, HangoutScreen.ROUTE);
-                                    }
-                                  }),
-                                  optionIcon(
-                                      'assets/icons/Compass.png', 'About', () {
-                                    Navigator.pushNamed(
-                                        context, AboutScreen.ROUTE);
-                                  }),
-                                ],
-                              ),
-                            ],
-                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
