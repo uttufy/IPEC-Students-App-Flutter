23709164a0129c83bb9aafd4599efd4c8cfad8fd
Utkarsh Sharma <utkarshstudent1@gmail.com>
2022-05-17 15:32:29 +0530

    some minor changes

--- lib/screens/about/about.dart
+++ lib/screens/about/about.dart
@@ -1,15 +1,28 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+
 import 'package:ipecstudentsapp/widgets/loading_widget.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
+import '../../theme/style.dart';
 import '../../widgets/simple_appbar.dart';
+import 'about_widget.dart';
 
 class AboutScreen extends StatelessWidget {
   static const String ROUTE = "/about";
+  _launchURL(String url) async {
+    print(url);
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    final isDark = AdaptiveTheme.of(context).mode == AdaptiveThemeMode.dark;
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -21,38 +34,55 @@ class AboutScreen extends StatelessWidget {
               title: "About",
             ),
             Expanded(
-              child: FutureBuilder<String>(
-                future: loadGistFileFromRemote(),
-                builder:
-                    (BuildContext context, AsyncSnapshot<String> snapshot) {
-                  print(snapshot.connectionState.toString());
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    print(snapshot.hasData);
-                    print(snapshot.hasError);
-                    if (snapshot.hasData)
-                      return Markdown(
-                        padding: const EdgeInsets.all(40),
-                        data: snapshot.data!,
-                        selectable: true,
-                        imageDirectory: 'https://raw.githubusercontent.com',
-                        onTapLink: (String text, String? href, String title) =>
-                            linkOnTapHandler(context, text, href ?? "", title),
-                      );
-                    else if (snapshot.hasError)
-                      return Center(
-                          child: Padding(
-                        padding: const EdgeInsets.all(30.0),
-                        child: Text(
-                          "Something went wrong! Please check your internet connectivity. ERROR : ${snapshot.error.toString()}",
-                          textAlign: TextAlign.center,
-                        ),
-                      ));
-                    else
-                      return Container();
-                  } else {
-                    return const LoadingWidget();
-                  }
-                },
+              child: SingleChildScrollView(
+                // physics: BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    kMedPadding,
+                    aboutWidget(
+                        context,
+                        'https://avatars.githubusercontent.com/u/16814257?v=4',
+                        'UTTU',
+                        'Utkarsh Sharma', () {
+                      _launchURL('https://www.instagram.com/uttufy');
+                    }, () {
+                      _launchURL('https://github.com/uttusharma');
+                    }, isDark),
+                    kMedPadding,
+                    aboutWidget(
+                        context,
+                        'https://avatars.githubusercontent.com/u/60356611?v=4',
+                        'Raks',
+                        'Rakshit Raj Singh', () {
+                      _launchURL(
+                          'https://www.instagram.com/rakshit.raj.singh/');
+                    }, () {
+                      _launchURL('https://github.com/RakshitRajSingh17');
+                    }, isDark),
+                    kMedPadding,
+                    aboutWidget(
+                        context,
+                        'https://avatars.githubusercontent.com/u/82357006?v=4',
+                        'Ridhi',
+                        'Ridhi Rawat', () {
+                      _launchURL('https://www.instagram.com/ri_aina2411');
+                    }, () {
+                      _launchURL('https://github.com/ridhi-7029hash');
+                    }, isDark),
+                    kHighPadding,
+                    Text(
+                      "Tap image to connect on instagram and name to connect on github",
+                      overflow: TextOverflow.clip,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                            color: isDark ? Colors.white : Colors.black,
+                          ),
+                    ),
+                    kMedPadding,
+                    _loadMdFile(),
+                  ],
+                ),
               ),
             )
           ],
@@ -61,16 +91,55 @@ class AboutScreen extends StatelessWidget {
     );
   }
 
+  Widget _loadMdFile() {
+    return FutureBuilder<String>(
+      future: loadGistFileFromRemote(),
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        // print(snapshot.connectionState.toString());
+        if (snapshot.connectionState == ConnectionState.done) {
+          // print(snapshot.hasData);
+          // print(snapshot.hasError);
+          if (snapshot.hasData)
+            return Markdown(
+              padding: const EdgeInsets.all(40),
+              shrinkWrap: true,
+              data: snapshot.data!,
+              selectable: true,
+              physics: NeverScrollableScrollPhysics(),
+              // imageDirectory: 'https://raw.githubusercontent.com',
+              onTapLink: (String text, String? href, String title) =>
+                  linkOnTapHandler(context, text, href ?? "", title),
+            );
+          else if (snapshot.hasError)
+            return Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Text(
+                "Something went wrong! Please check your internet connectivity. ERROR : ${snapshot.error.toString()}",
+                textAlign: TextAlign.center,
+              ),
+            );
+          else
+            return Container();
+        } else {
+          return Center(
+            child: SizedBox(
+                width: 200,
+                height: 200,
+                child: const CircularProgressIndicator()),
+          );
+        }
+      },
+    );
+  }
+
   Future<String> loadGistFileFromRemote() async {
     final http.Response response = await http.get(Uri.parse(
+        // 'https://gist.githubusercontent.com/uttusharma/8ffa1b2040a907279cd35ef79038c223/raw/ebce8dff7d6b3da481abfe0cc6d982a8fd02e6e0/IPEC-APP-INFO-BOARD'));
         'https://gist.githubusercontent.com/uttusharma/8ffa1b2040a907279cd35ef79038c223/raw'));
-    // print(response.body);
+    print(response.body);
     return response.body;
   }
 
-// Handle the link. The [href] in the callback contains information
-// from the link. The url_launcher package or other similar package
-// can be used to execute the link.
   Future<void> linkOnTapHandler(
     BuildContext context,
     String text,
---
+++ lib/screens/about/about_widget.dart
@@ -0,0 +1,70 @@
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
+import 'package:flutter/material.dart';
+
+import '../../theme/style.dart';
+
+Column aboutWidget(
+  BuildContext context,
+  String url,
+  String intial,
+  String name,
+  Function ontap,
+  Function onTap2,
+  bool isDark,
+) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      profileImage(url, intial, ontap),
+      kMedPadding,
+      GestureDetector(
+        onTap: () {
+          onTap2();
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              name,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                  color: isDark ? Colors.white : Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "I.T Batch 2019-2022",
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                    color: isDark ? Colors.white : Colors.black,
+                  ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget profileImage(String image, String intial, Function onTap) {
+  return CircularProfileAvatar(
+    image, //sets image path, it should be a URL string. default value is empty string, if path is empty it will display only initials
+    radius: 100, // sets radius, default 50.0
+    backgroundColor: Colors.blue, // sets background color, default Colors.white
+    borderWidth: 2, // sets border, default 0.0
+    initialsText: Text(
+      intial,
+      style: TextStyle(fontSize: 40, color: Colors.white),
+    ), // sets initials text, set your own style, default Text('')
+    // borderColor: Colors.blue, // sets border color, default Colors.white
+    elevation:
+        5.0, // sets elevation (shadow of the profile picture), default value is 0.0
+
+    cacheImage: true, // allow widget to cache image against provided url
+    onTap: () {
+      onTap();
+    }, // sets on tap
+
+    showInitialTextAbovePicture:
+        false, // setting it true will show initials text above profile picture, default false
+  );
+}
